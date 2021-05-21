742d2145d8e39ebf9925daf673bdbccb499cfd07
gnishiwaki <gen.nishiwaki@gmail.com>
2021-05-20 23:49:27 -0400

    Improved player object slightly more

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let player = {
+    name: "Per",
+    chips: 200
+}
+
 let cards =[]
 let sum = 0
 let hasBlackJack = false
@@ -5,13 +10,7 @@ let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
-
 let cardsEl = document.getElementById("cards-el")
-
-let player = {
-    name: "Per",
-    chips: 145
-}
 
 let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + ": $" + player.chips
