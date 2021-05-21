9d3291c5331c9a01ee3abc0208075570cbe6e574
gnishiwaki <gen.nishiwaki@gmail.com>
2021-05-20 20:37:53 -0400

    Made the start button

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw another card?"
-} else if (sum === 21) {
-    message = "BLACKJACK!!! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're over! 😭"
-    isAlive = false
+function startGame(){
+    if (sum <= 20) {
+        message = "Do you want to draw another card?"
+    } 
+    else if (sum === 21) {
+        message = "BLACKJACK!!! 🥳"
+        hasBlackJack = true
+    } 
+    else {
+        message = "You're over! 😭"
+        isAlive = false
+    }
 }
+
 
