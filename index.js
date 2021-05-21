915131fcc356e1eef4948e1cbb45ef8d20f105e0
gnishiwaki <gen.nishiwaki@gmail.com>
2021-05-20 23:41:25 -0400

    Made player object

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ let sumEl = document.querySelector("#sum-el")
 
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "Per",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
